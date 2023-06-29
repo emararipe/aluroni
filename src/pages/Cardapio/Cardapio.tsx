c8a5753a782e26f4988c1ee2395d7274109f801f
emararipe <emararipe@gmail.com>
2023-06-29 20:09:14 -0300

    início do projeto

---
+++ src/pages/Cardapio/Cardapio.tsx
@@ -0,0 +1,12 @@
+import styles from './Cardapio.module.scss'
+
+function Cardapio() {
+  return (
+    <main>
+      <nav className='menu'>
+      </nav>
+    </main>
+  )
+}
+
+export default Cardapio
