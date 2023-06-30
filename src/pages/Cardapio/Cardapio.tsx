718b612ed377b74013756d35b1bf99f42f2b296b
emararipe <emararipe@gmail.com>
2023-06-29 23:07:54 -0300

    normalize css + header

--- src/pages/Cardapio/Cardapio.tsx
+++ src/pages/Cardapio/Cardapio.tsx
@@ -1,3 +1,4 @@
+import Buscador from './Buscador/Buscador'
 import styles from './Cardapio.module.scss'
 import { ReactComponent as Logo } from 'assets/logo.svg'
 
@@ -5,8 +6,17 @@ function Cardapio() {
   return (
     <main>
       <nav className={styles.menu}>
-       <Logo />
+        <Logo />
       </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
+          A casa do código e da massa
+        </div>
+      </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+        <Buscador />
+      </section >
     </main>
   )
 }
