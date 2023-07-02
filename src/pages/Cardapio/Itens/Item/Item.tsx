6d9ed405a7dbdd6dd98470bbcc8add0af25c6fe8
emararipe <emararipe@gmail.com>
2023-07-02 15:21:59 -0300

    finalização itens do cardápio + aplicação filtros e busca

--- src/pages/Cardapio/Itens/Item/Item.tsx
+++ src/pages/Cardapio/Itens/Item/Item.tsx
@@ -1,29 +1,40 @@
-import React from 'react'
+import classNames from 'classnames'
 import styles from './Item.module.scss'
 import logo from 'assets/logo.svg'
+import { type } from 'os'
+import itens from '../itens.json'
 
+type Props = typeof itens[0]
 
-function Item() {
+function Item(props: Props) {
+  const { ...itens } = props
   return (
     <div className={styles.item}>
       <div className={styles.item__imagem} >
-        <img src={logo} alt="completar" />
+        <img src={itens.photo} alt="completar" />
       </div>
       <div className={styles.item__descricao}>
         <div className={styles.item__titulo}>
-          <h2>Massa</h2>
-          <p>Descrição do prato</p>
+          <h2>{itens.title}</h2>
+          <p>{itens.description}</p>
         </div>
         <div className={styles.item__tags}>
-          <div className={styles.item__tipo}> Massa </div>
-          <div className={styles.item__porcao}> 400g </div>
-          <div className={styles.item__qtdpessoas}> 2 </div>
-          <div className={styles.item__valor}> R$ 70,00 </div>
+          <div 
+          className={classNames(
+              styles.item__tipo,
+              styles[`item__tipo__${itens.category.label.toLowerCase()}`]
+            )}
+          >
+            {itens.category.label}
+          </div>
+          <div className={styles.item__porcao}> {itens.size}g </div>
+          <div className={styles.item__qtdpessoas}> {itens.serving} </div>
+          <div className={styles.item__valor}> R${itens.price.toFixed(2)} </div> {/* formatação preço item */}
         </div>
       </div>
     </div>
   )
-  
+
 }
 
 export default Item
--- src/pages/Cardapio/Itens/Itens.tsx
+++ src/pages/Cardapio/Itens/Itens.tsx
@@ -1,12 +1,44 @@
+import { useState, useEffect } from 'react'
 import Item from './Item/Item'
 import itens from './itens.json'
 import styles from './Itens.module.scss'
 
-function Itens() {
+interface Props {
+  busca: string,
+  filtro: number | null,
+  ordenador: string
+}
+
+function Itens(props: Props) {
+  const { busca, filtro } = props
+  const [lista, setLista] = useState(itens)
+
+  function testaBusca(title: string) {
+    const regex = new RegExp(busca, 'i')
+    return regex.test(title)
+  }
+
+  function testaFiltro(id: number) {
+    if (filtro !== null) return filtro === id
+    return true
+  }
+
+  useEffect(() => {
+    const novaLista = itens.filter((item) => (
+      testaBusca(item.title) &&
+      testaFiltro(item.category.id)
+    ))
+    setLista(novaLista)
+  }, [busca, filtro])
+
+
   return (
     <div className={styles.itens}>
-      {itens.map((item) => (
-        <Item key={item.id}/>
+      {lista.map((item) => (
+        <Item
+          key={item.id}
+          {...item}
+        />
       ))}
     </div>
   )
