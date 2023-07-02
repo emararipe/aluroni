6da9b6bc2d297b6adc7e145d86bc81c020d607b8
emararipe <emararipe@gmail.com>
2023-07-02 15:39:31 -0300

    finalização função ordenador

--- src/pages/Cardapio/Itens/Itens.tsx
+++ src/pages/Cardapio/Itens/Itens.tsx
@@ -9,8 +9,7 @@ interface Props {
   ordenador: string
 }
 
-function Itens(props: Props) {
-  const { busca, filtro } = props
+function Itens({ busca, filtro, ordenador }: Props) {
   const [lista, setLista] = useState(itens)
 
   function testaBusca(title: string) {
@@ -23,13 +22,26 @@ function Itens(props: Props) {
     return true
   }
 
+  function ordenarLista(lista: typeof itens){
+    switch(ordenador){
+      case 'porcao':
+        return lista.sort((a, b) => a.size > b.size ? 1 : -1 )
+      case 'qtd_pessoas':
+        return lista.sort((a, b) => a.serving > b.serving ? 1 : -1 )
+      case 'preco':
+        return lista.sort((a, b) => a.price > b.price ? 1 : -1 )
+      default: 
+        return lista
+    }
+  }
+
   useEffect(() => {
     const novaLista = itens.filter((item) => (
       testaBusca(item.title) &&
       testaFiltro(item.category.id)
     ))
-    setLista(novaLista)
-  }, [busca, filtro])
+    setLista(ordenarLista(novaLista))
+  }, [busca, filtro, ordenador])
 
 
   return (
