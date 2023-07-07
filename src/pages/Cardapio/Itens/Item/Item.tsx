600fcfdbce25c61151e55895275b4b3ff0eba31f
emararipe <emararipe@gmail.com>
2023-07-07 20:35:55 -0300

    Update: pags item itens redirecionam para pag pratos detalhada

--- src/pages/Cardapio/Itens/Item/Item.tsx
+++ src/pages/Cardapio/Itens/Item/Item.tsx
@@ -1,13 +1,15 @@
 import TagPratos from 'components/TagPratos/TagPratos'
 import styles from './Item.module.scss'
 import { Prato } from 'types/Pratos'
+import { useNavigate } from 'react-router-dom'
 
 type Props = Prato
 
 function Item(props: Props) {
 	const { ...itens } = props
+  const navigate = useNavigate()
 	return (
-		<div className={styles.item}>
+		<div className={styles.item} onClick={() => navigate(`/prato/${itens.id}`)}>
 			<div className={styles.item__imagem} >
 				<img src={itens.photo} alt='completar' />
 			</div>
